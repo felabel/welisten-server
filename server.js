8aedf559fef874a8c6e6c1671346fca09858c838
felabel <felicityabel2016@gmail.com>
2025-01-29 08:54:17 +0100

    removed routes

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ import { register, login } from "./routes/auth.js";
 const PORT = process.env.PORT || 3000;
 
 const server = createServer((req, res) => {
-  res.setHeader("Content-Type", "application/json");
+  res.end("we listen server dey run go where e no know");
+  // res.setHeader("Content-Type", "application/json");
 
-  if (req.method === "POST" && req.url === "/register") {
-    register(req, res);
-  } else if (req.method === "POST" && req.url === "/login") {
-    login(req, res);
-  } else {
-    res.writeHead(404);
-    res.end(JSON.stringify({ error: "Route not found" }));
-  }
+  // if (req.method === "POST" && req.url === "/register") {
+  //   register(req, res);
+  // } else if (req.method === "POST" && req.url === "/login") {
+  //   login(req, res);
+  // } else {
+  //   res.writeHead(404);
+  //   res.end(JSON.stringify({ error: "Route not found" }));
+  // }
 });
 
 server.listen(PORT, () => {
