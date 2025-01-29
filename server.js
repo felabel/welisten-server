139f6d1f549a19908ac099c2becb2e862806ec1e
felabel <felicityabel2016@gmail.com>
2025-01-29 09:11:02 +0100

    reverted code

--- server.js
+++ server.js
@@ -1,26 +1,11 @@
 import { createServer } from "http";
-import { register } from "./routes/auth.js";
-
-const PORT = process.env.PORT || 3000;
 
 const server = createServer((req, res) => {
-  // res.end("we listen server dey run go where e no know");
-  console.log("req", req);
-  res.end(req);
-  res.setHeader("Content-Type", "application/json");
-
-  // if (req.method === "POST" && req.url === "/register") {
-  //   register(req, res);
-  // }
-  //  else if (req.method === "POST" && req.url === "/login") {
-  //   login(req, res);
-  // }
-  // else {
-  //   res.writeHead(404);
-  //   res.end(JSON.stringify({ error: "Route not found" }));
-  // }
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "WeListen Backend is Running!" }));
 });
 
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
